846ab274c16a492eacd9a2145549f6de2553cccc
Lisa Swart <arnoudlisa@gmail.com>
2021-01-19 07:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log.toLowerCase()
+}
